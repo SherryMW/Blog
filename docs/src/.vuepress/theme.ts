be8eff12f54ea51eb8e2f2be0f6c8a6b060ebba8
MW <sherry_mw@163.com>
2022-12-06 01:07:10 +0800

    更新ECS文章

--- docs/src/.vuepress/theme.ts
+++ docs/src/.vuepress/theme.ts
@@ -47,8 +47,6 @@ export default hopeTheme({
 
     editLink: true, //是否展示编辑此页链接
 
-    hotReload: true, //是否为需要重启整个 app 的功能启用热更新
-
     blog: {
         avatar: "/avatar.jpg", //头像
         roundAvatar: true, //是否剪裁头像为圆形形状
