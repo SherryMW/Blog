a1128141fee9d0f66e6e10b3972d3e216c2a40e8
MW <sherry_mw@163.com>
2023-01-08 12:58:49 +0800

    更新文章

--- docs/src/.vuepress/theme.ts
+++ docs/src/.vuepress/theme.ts
@@ -2,6 +2,7 @@ import {hopeTheme} from "vuepress-theme-hope";
 import {Navbar} from "./bar/navbar";
 import {Sidebar} from "./bar/sidebar";
 
+process.env.TZ = "GMT+8";
 const data = await fetch("https://api.github.com/repos/SherryMW/Blog?token=ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGZrbd1zYgX7b4FYXUpAxWEjDOIJAYsuyjIuAEhKeove sherry_mw@163.com").then(response => response.json());
 const lastUpdate = new Date(data['pushed_at']).getFullYear().toString() + "年" + (new Date(data['pushed_at']).getMonth() + 1).toString() + "月" + new Date(data['pushed_at']).getDate().toString() + "日 " + new Date(data['pushed_at']).getHours() + ":" + new Date(data['pushed_at']).getMinutes();
 
