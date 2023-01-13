6886ca6ffe690195a59a5f6008974c39c208ebf9
MW <sherry_mw@163.com>
2023-01-13 12:01:40 +0800

    docs: update config

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -8,14 +8,6 @@ export default defineUserConfig({
     description: "MW Blog",
     head: [
         ["meta", {name: "google-site-verification", content: "bqJ9p_CBwVm2NILF0O4wil3tvCMGYwHdmDUYU6QQNwA"}],
-        ['script', {}, `
-        var _hmt = _hmt || [];
-        (function() {
-            var hm = document.createElement('script');
-            hm.src = 'https://hm.baidu.com/hm.js?f920f2b7a642efd7744e052f2c9b7152';
-            var s = document.getElementsByTagName('script')[0]; 
-            s.parentNode.insertBefore(hm, s);
-        })();`]
     ],
     plugins: [],
 
