36749c90888dfa40289cb9c977ea3ad6f711624f
MW <sherry_mw@163.com>
2023-02-19 00:26:20 +0800

    docs: 更新文章

--- docs/src/.vuepress/config/navbar.ts
+++ docs/src/.vuepress/config/navbar.ts
@@ -6,25 +6,25 @@ export const Navbar = navbar([
         icon: "home",
         link: "/",
     },
-    {
-        text: "生活",
-        icon: "emoji",
-        prefix: "/life/",
-        children: [
-            {
-                text: "健康",
-                link: "healthy/"
-            },
-            {
-                text: "饮食",
-                link: "delicacies/"
-            },
-            {
-                text: "娱乐",
-                link: "play/guangzhou/"
-            }
-        ]
-    },
+    // {
+    //     text: "生活",
+    //     icon: "emoji",
+    //     prefix: "/life/",
+    //     children: [
+    //         {
+    //             text: "健康",
+    //             link: "healthy/"
+    //         },
+    //         {
+    //             text: "饮食",
+    //             link: "delicacies/"
+    //         },
+    //         {
+    //             text: "娱乐",
+    //             link: "play/guangzhou/"
+    //         }
+    //     ]
+    // },
     {
         text: "链接",
         icon: "link",
