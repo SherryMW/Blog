d64cdb0e043ec403ee1c15e7e0be22a9539e20f0
MW <sherry_mw@163.com>
2024-01-16 17:51:10 +0800

    配置

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -2,7 +2,6 @@ import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 import {containerPlugin} from "@vuepress/plugin-container";
 import {renderProjects} from "./config/projects";
-// import {docsearchPlugin} from "@vuepress/plugin-docsearch";
 import {searchProPlugin} from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
@@ -29,54 +28,6 @@ export default defineUserConfig({
             }
         }),
         searchProPlugin({}),
-        // docsearchPlugin({
-        //     appId: "3BWRIKCJ5K",
-        //     apiKey: "69ab740886a5bcb9703dfa42349119f0",
-        //     indexName: "sherry4869",
-        //     locales: {
-        //         "/": {
-        //             placeholder: "搜索",
-        //             translations: {
-        //                 button: {
-        //                     buttonText: "搜索",
-        //                     buttonAriaLabel: "搜索",
-        //                 },
-        //                 modal: {
-        //                     searchBox: {
-        //                         resetButtonTitle: "清除查询条件",
-        //                         resetButtonAriaLabel: "清除查询条件",
-        //                         cancelButtonText: "取消",
-        //                         cancelButtonAriaLabel: "取消",
-        //                     },
-        //                     startScreen: {
-        //                         recentSearchesTitle: "搜索历史",
-        //                         noRecentSearchesText: "没有搜索历史",
-        //                         saveRecentSearchButtonTitle: "保存至搜索历史",
-        //                         removeRecentSearchButtonTitle: "从搜索历史中移除",
-        //                         favoriteSearchesTitle: "收藏",
-        //                         removeFavoriteSearchButtonTitle: "从收藏中移除",
-        //                     },
-        //                     errorScreen: {
-        //                         titleText: "无法获取结果",
-        //                         helpText: "你可能需要检查你的网络连接",
-        //                     },
-        //                     footer: {
-        //                         selectText: "选择",
-        //                         navigateText: "切换",
-        //                         closeText: "关闭",
-        //                         searchByText: "搜索提供者",
-        //                     },
-        //                     noResultsScreen: {
-        //                         noResultsText: "无法找到相关结果",
-        //                         suggestedQueryText: "你可以尝试查询",
-        //                         reportMissingResultsText: "你认为该查询应该有结果？",
-        //                         reportMissingResultsLinkText: "点击反馈",
-        //                     },
-        //                 },
-        //             },
-        //         },
-        //     },
-        // }),
     ],
 
     theme,
